c723627ea29769daff5152e334218e69c4d93d81
Philipe-Berube <philipe.berube@hotmail.com>
2026-03-20 11:37:15 -0400

    first commit

---
+++ oopfinalproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace oopfinalproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
