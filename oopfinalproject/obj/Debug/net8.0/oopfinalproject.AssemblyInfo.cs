886d4583aa8a4f12fe4bb13f971882a2c108e729
alex de sousa <thegamerisbacon@gmail.com>
2026-03-28 10:02:01 -0400

    created ISortable

--- oopfinalproject/obj/Debug/net8.0/oopfinalproject.AssemblyInfo.cs
+++ oopfinalproject/obj/Debug/net8.0/oopfinalproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oopfinalproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a621ef59a4166feb17599144aac4acee54916322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc23a20ca245e088d08c8966af32f6fcbaf15645")]
 [assembly: System.Reflection.AssemblyProductAttribute("oopfinalproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oopfinalproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
