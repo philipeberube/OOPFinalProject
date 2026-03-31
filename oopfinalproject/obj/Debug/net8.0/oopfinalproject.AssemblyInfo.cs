ccaa9972b1a653ed253e065da7e6a5c9ae0d65bd
Philipe-Berube <philipe.berube@hotmail.com>
2026-03-31 10:54:32 -0400

    a

--- oopfinalproject/obj/Debug/net8.0/oopfinalproject.AssemblyInfo.cs
+++ oopfinalproject/obj/Debug/net8.0/oopfinalproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oopfinalproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bfda5e2d6d9064a05b6b45d5db253aee7349a88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c547325936e6de441f3c3209e10e3de6f90c46e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("oopfinalproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oopfinalproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
