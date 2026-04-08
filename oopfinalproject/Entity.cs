b464c3392fcd0d5212f82c1c805aa01b126945bb
alex de sousa <thegamerisbacon@gmail.com>
2026-04-08 09:05:43 -0400

    some validations in entity

--- oopfinalproject/Entity.cs
+++ oopfinalproject/Entity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -53,10 +54,65 @@ namespace oopfinalproject
             return name;
         }
 
-        public virtual bool Validate()
+        public virtual bool Validate(string val)
         {
+            switch (val)
+            {
+                case "id":
+                    if (id <= 0)
+                    {
+                        throw new ArgumentException("id cannot be less then 0 or 0 ");
+                    }
+                    if(id >= 5000)
+                    {
+                        throw new ArgumentException("id cannot be greater than 5000");
+                    }
+                    if(id == null)
+                    {
+                        throw new ArgumentException("id cannot be null");
+                    }
+                    break;
+                case "name":
+                    if (string.IsNullOrEmpty(name))
+                    {
+                        throw new ArgumentException("name cannot be empty");
+                    }
+                    for (int i = 0; i < name.Length; i++)
+                    {
+                        if (!char.IsLetter(name[i]) && !char.IsWhiteSpace(name[i]))
+                        {
+                            throw new ArgumentException("name can only contain letters and spaces");
+                        }
+                    }
+                    for(int i = 0; i < name.Length; i++)
+                    {
+                        if (char.IsWhiteSpace(name[i]) && char.IsWhiteSpace(name[i + 1]))
+                        {
+                            throw new ArgumentException("name connot contain multiple spaces ");
+                        }
+                    }
+                    for(int i = 0; i < name.Length; i++)
+                    {
+                        if (char.IsWhiteSpace(name[0]) || char.IsWhiteSpace(name[name.Length - 1]))
+                        {
+                            throw new ArgumentException("name cannot start or end with a space");
+                        }
+                    }
+                    break;
+                case "createdDate":
+                    if (createdDate == null)
+                    {
+                        throw new ArgumentException("date cannot be null");
+                    }
+                    if(createdDate > DateTime.Now)
+                    {
+                        throw new ArgumentException("date cannot be in the future");
+                    }
+                    break;
+
+            }
             return true;
-        }//validate?
+        }
   
 
         public abstract void Display();
